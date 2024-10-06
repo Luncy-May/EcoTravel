ac68dde01f95e918f900a9e805d0db3ba32c5fce
Luncy-May <cheng.li1@wustl.edu>
2024-10-06 13:59:58 -0500

    init setup

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,25 @@
 import NotAuthorizedPage from "./NotAuthorizedPage";
 import Report from "./Report";
+import About from "./About";
+import Calendar from "./Calendar";
+import Community from "./Community";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import MyPlans from "./MyPlans";
+import Profile from "./Profile";
+import Report from "./Report";
+import Setting from "./Setting";
+import Team from "./Team";
 export {
     NotAuthorizedPage,
-    Report
+    Report,
+    About,
+    Calendar,
+    Community,
+    Home,
+    Dashboard,
+    MyPlans,
+    Profile,
+    Setting,
+    Team
 }
