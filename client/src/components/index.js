68237b331386b99f1e0a3aba11f226d1dae9c94d
Luncy-May <cheng.li1@wustl.edu>
2024-10-07 23:00:24 -0500

    update

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,6 +6,8 @@ import BackgroundImage from "./BackgroundImage";
 import CityState from "./CityState";
 import Navbar from "./Navbar";
 import NavbarContents from "./NavbarContents";
+import StateInfo from "./StateInfo";
+import ImageSwapper from "./ImageSwapper";
 export {
     Plan,
     Weather,
@@ -14,5 +16,7 @@ export {
     BackgroundImage,
     CityState,
     Navbar,
-    NavbarContents
+    NavbarContents,
+    StateInfo,
+    ImageSwapper
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,51 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaSignInAlt, FaUserPlus, FaSearch } from 'react-icons/fa';
+import { BackgroundImage } from '../components';
 const About = () => {
   return (
     <div>
       <div className="relative w-full h-full flex flex-col items-center justify-center overflow-hidden">
-        <div className="text-black text-[140px] font-inter pt-[5vh] overflow-hidden whitespace-nowrap">Our Missions</div>
-        <div className='text-black overflow-hidden space-x-5 flex items-center justify-center font-inter '>
-          <div>
+        <div className='absolute w-full h-full z-10 brightness-75'>
+          <BackgroundImage name="about" imagePath={"/travel.jpg"} />
+        </div>
+        <div className="text-gray-300 z-50 text-[120px] font-inter overflow-hidden whitespace-nowrap">Our Missions</div>
+        <div className='text-gray-300 overflow-hidden flex items-center justify-center font-inter '>
+          <div className='z-20'>
             <div className='flex-shrink-0'>
-              <Link to="/login">
-                <div className='p-5 text-2xl font-bold shadow-md hover:shadow-2xl min-h-[300px] min-w-[300px] flex flex-col items-center justify-center break-words'>
-                  <div className='mb-2'>
-                    <FaSignInAlt size={50} />
-                  </div>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
                   <div className='mt-2'>
-                    Already have an account? Login here!
+                  Eco-Friendly Travel: Offer users a curated selection of eco-lodges, sustainable tour operators, and environmentally responsible transportation options, ensuring a guilt-free travel experience.
                   </div>
                 </div>
-              </Link>
             </div>
 
             <div className='flex-shrink-0'>
-              <Link to="/register">
-                <div className='p-5 text-2xl font-bold shadow-md hover:shadow-2xl min-h-[300px] min-w-[300px] flex flex-col items-center justify-center break-words'>
-                  <div className='mb-2'>
-                    <FaUserPlus size={50} />
-                  </div>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
                   <div className='mt-2'>
-                    Register an account. It is free!
+                  Social Good: Encourage users to participate in eco-friendly initiatives, conservation projects, and social enterprises that benefit local communities and contribute to the greater good.
                   </div>
                 </div>
-              </Link>
+            </div>
+            <div className='flex-shrink-0'>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
+                  <div className='mt-2'>
+                  Social Good: Inspire users to participate in volunteer opportunities, community service projects, and eco-friendly initiatives that benefit local communities and contribute to the greater good.
+                  </div>
+                </div>
+            </div>
+            <div className='flex-shrink-0'>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
+                  <div className='mt-2'>
+                  Personalized Recommendations: Provide tailored travel recommendations, activity suggestions, and insider tips based on users' interests, preferences, and travel style, ensuring a unique and enriching experience.
+                  </div>
+                </div>
+            </div>
+            <div className='flex-shrink-0'>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
+                  <div className='mt-2'>
+                  Education and Awareness: Raise awareness about environmental and social issues affecting travel destinations, promoting responsible travel practices and sustainable tourism.
+                  </div>
+                </div>
+            </div>
+            <div className='flex-shrink-0'>
+                <div className='p-5 text-2xl font-bold flex flex-col items-center justify-center break-words'>
+                  <div className='mt-2'>
+                  Incentives and Rewards: Recognize and reward users for their eco-friendly travel choices, community contributions, and social good achievements, motivating them to continue making a positive impact.
+                  </div>
+                </div>
             </div>
           </div>
-          <div className='flex-shrink-0'>
-            <Link to="/community">
-              <div className='pl-5 pr-5 text-2xl font-bold shadow-md hover:shadow-2xl min-h-[300px] min-w-[300px] flex flex-col items-center justify-center break-words'>
-                <div className='mb-2'>
-                  <FaSearch size={50} />
-                </div>
-                <div className='mt-2'>
-                  Check out what other amazing people are doing with OurCity here!
-                </div>
-              </div>
-            </Link>
-          </div>
+          
         </div>
 
       </div>
